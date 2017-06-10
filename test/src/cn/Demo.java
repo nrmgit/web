9ba485afa4dce9b5139042288be100b1bf1f8d6e
nrmgit <2293650260@qq.com>
2017-06-10 14:54:18 +0800

    liyou

---
+++ test/src/cn/Demo.java
@@ -0,0 +1,16 @@
+package cn;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("己二胺；发附件啊");
+			System.out.println("己二胺；发附件啊");
+			System.out.println("己二胺；发附件啊");
+
+	}
+
+}
